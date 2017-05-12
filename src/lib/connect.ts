cef769456614429bd3e977f7a76a56a73de0f203
Mitch Capper <mitch.capper@gmail.com>
2017-05-11 22:10:44 -0700

    save/load state support (somewhat hacky) I save out everything but only end up using skypeToken as could not get to work without re-creating the endpoint.

--- src/lib/connect.ts
+++ src/lib/connect.ts
@@ -1,8 +1,11 @@
-import {Incident} from "incident";
+﻿ import {Incident} from "incident";
+import { CookieJar } from "request";
+import * as request from "request";
+import { CookieJar as CookieJar2, MemoryCookieStore } from "tough-cookie";
 import * as api from "./api";
 import {Credentials} from "./interfaces/api/api";
 import {Context} from "./interfaces/api/context";
-import {login} from "./login";
+import {login, loginResume} from "./login";
 import requestIO from "./request-io";
 
 export interface StateContainer {
@@ -11,10 +14,16 @@ export interface StateContainer {
 
 export interface ConnectOptions {
   credentials?: Credentials;
-  state?: any;
+  state?: StateObj;
   verbose?: boolean;
 }
 
+export interface StateObj {
+  skypeToken: string;
+  registrationToken: string;
+  username: string;
+  cookieJar: string;
+}
 /**
  * Authenticate the user and create a new API.
  *
@@ -23,23 +32,37 @@ export interface ConnectOptions {
  * @throws [[LoginError]]
  */
 export async function connect(options: ConnectOptions): Promise<api.Api> {
+  let apiContext: Context;
   if (options.state !== undefined) {
-    return Promise.reject(new Incident("todo", "Connection from previous state is not yet supported."));
-  } else if (options.credentials === undefined) {
-    return Promise.reject(new Incident("todo", "Connect must define `credentials`"));
-  }
-  const apiContext: Context = await login({
-    io: requestIO,
-    credentials: options.credentials,
-    verbose: options.verbose,
-  });
-  if (options.verbose) {
-    console.log("Obtained context trough authentication:");
+    const store: MemoryCookieStore = new MemoryCookieStore();
+    CookieJar2.deserializeSync(options.state.cookieJar, store);
+    request.defaults({ jar: request.jar(store) });
+    apiContext = await loginResume({  //  registrationToken: JSON.parse(options.state.registrationToken)
+      io: requestIO, cookieStore: store, cookieJar: request.jar(store),
+      skypeToken: JSON.parse(options.state.skypeToken),
+      registrationToken: null, verbose: options.verbose, username: options.state.username});
     console.log({
       username: apiContext.username,
       skypeToken: apiContext.skypeToken,
       registrationToken: apiContext.registrationToken,
     });
+  } else if (options.credentials) {
+    apiContext = await login({
+      io: requestIO,
+      credentials: options.credentials,
+      verbose: options.verbose,
+    });
+    if (options.verbose) {
+      console.log("Obtained context trough authentication:");
+      console.log({
+        username: apiContext.username,
+        skypeToken: apiContext.skypeToken,
+        registrationToken: apiContext.registrationToken,
+      });
+    }
+
+  } else {
+    return Promise.reject(new Incident("todo", "Connect must define `credentials`"));
   }
   return new api.Api(apiContext, requestIO);
 }
