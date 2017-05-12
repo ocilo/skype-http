f13d87e0df31535a68cc31960fdc1f299611604d
Mitch Capper <mitch.capper@gmail.com>
2017-05-12 18:13:49 -0500

    Fixed get-contact to work with the new endpoint.     Note the new endpoint does not return as much data as the normal profile list and only works with skype contacts.

--- src/lib/api/get-contact.ts
+++ src/lib/api/get-contact.ts
@@ -1,8 +1,9 @@
-import {Incident} from "incident";
+﻿ import {Incident} from "incident";
 import * as apiUri from "../api-uri";
 import {Contact} from "../interfaces/api/contact";
 import {Context} from "../interfaces/api/context";
 import * as io from "../interfaces/http-io";
+import { formatSearchContact } from "../utils/formatters";
 
 export const VIRTUAL_CONTACTS: Set<string> = new Set(["concierge", "echo123"]);
 
@@ -11,23 +12,19 @@ export async function getContact(io: io.HttpIo, apiContext: Context, contactId: 
     // tslint:disable-next-line:max-line-length
     throw new Error(`${JSON.stringify(contactId)} is not a real contact, you cannot get data for ${JSON.stringify(contactId)}`);
   }
-  // concierge
-  console.log(`Getting contact: ${contactId}`);
-  const requestOptions: io.GetOptions = {
-    uri: apiUri.userProfile(contactId),
+  const requestOptions: io.PostOptions = {
+    uri: apiUri.userProfiles(),
     jar: apiContext.cookieJar,
+    form: { usernames: [contactId] },
     headers: {
       "X-Skypetoken": apiContext.skypeToken.value,
     },
   };
-  const res: io.Response = await io.get(requestOptions);
-
-  console.log(`Response for contact ${JSON.stringify(contactId)}:`);
-  console.log(res);
+  const res: io.Response = await io.post(requestOptions);
   if (res.statusCode !== 200) {
     return Promise.reject(new Incident("net", "Unable to fetch contact"));
   }
-  const body: Contact = JSON.parse(res.body);
+  const body: Contact = formatSearchContact(JSON.parse(res.body)[0]);
   return body;
 }
 
